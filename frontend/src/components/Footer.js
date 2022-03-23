1d8f17e57375bc9fedc6fee5327fd292f05a975d
Shimanshu yadav <shimanshuyadav83@gmail.com>
2022-03-23 21:13:08 +0530

    done

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
   return (
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+          <Col className="text-center py-3">
+            Copyright &copy; Super Grocery{" "}
+          </Col>
+          <Col className="text-center py-3"> devoped by Shimanshu Yadav </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
