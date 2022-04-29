4f2a554a9504a55ed279615a081ba5cd977b6444
Shimanshu yadav <shimanshuyadav83@gmail.com>
2022-04-29 23:02:20 +0530

    first commit

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -14,7 +14,7 @@ const PaymentScreen = ({ history }) => {
   }
 
   const [paymentMethod, setPaymentMethod] = useState("PayPal");
-
+  
   const dispatch = useDispatch();
 
   const submitHandler = (e) => {
